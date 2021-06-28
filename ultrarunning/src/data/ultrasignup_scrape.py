f39fa4df3d24dc9496a6c50d9be5a6d8a0e5d2d4
mkrueger12 <max@kruegers.com>
2021-06-27 20:10:14 -0600

    feat: get_enterants func

--- ultrarunning/src/data/ultrasignup_scrape.py
+++ ultrarunning/src/data/ultrasignup_scrape.py
@@ -1,17 +1,27 @@
 import pandas as pd
+import numpy as np
 import requests
 import io
 
 
 def get_eventId(dtid_id):
 
-
     url = f'https://ultrasignup.com/register.aspx?dtid={dtid_id}'
     resp = requests.get(url=url)
     event_id = resp.url[-5:]
+    dict = {'EventDateId': dtid_id, 'EventID': event_id}
     print('Conversion of', dtid_id, 'to', event_id, 'Completed.')
 
-    return dtid_id
+    return dict
+
+def get_entrants(event_id, event_data):
+
+    event_data = event_data[['EventDateId', 'EventId', '']]
+
+    resp = requests.get(url=f'https://ultrasignup.com/entrants_event.aspx?did={event_id}')
+    df = pd.read_html(resp.content)
+    df = pd.DataFrame(df[0])
+    df['Ev']
 
 
 #scrape data
@@ -25,10 +35,11 @@ data = pd.DataFrame(data)
 
 # get eventId
 event_id = [*map(get_eventId, data['EventDateId'])]
+event_id = pd.DataFrame(event_id)
+data = pd.merge(event_id, data)
 
 # get entrant list
 resp = requests.get(url='https://ultrasignup.com/entrants_event.aspx?did=78606')
-df = pd.read_csv(io.BytesIO(resp.content), error_bad_lines=False)
-df = pd.read_html(df)
-data = resp.json()
+df = pd.read_html(resp.content)
+df = pd.DataFrame(df[0])
 
