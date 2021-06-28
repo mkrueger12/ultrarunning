de19dfa730bfd9542b8291c0a5c4bf1e5ed4198b
mkrueger12 <max@kruegers.com>
2021-06-27 20:26:37 -0600

    chore: update code

--- ultrarunning/src/data/ultrasignup_scrape.py
+++ ultrarunning/src/data/ultrasignup_scrape.py
@@ -14,15 +14,25 @@ def get_eventId(dtid_id):
 
     return dict
 
-def get_entrants(event_id, event_data):
+def get_entrants(data, event_id):
 
-    event_data = event_data[['EventDateId', 'EventId', '']]
+    data = data[['EventDateId', 'EventId', 'EventName', 'EventDate']].drop_duplicates()
+    data = data[data[0] == event_id]
 
     resp = requests.get(url=f'https://ultrasignup.com/entrants_event.aspx?did={event_id}')
     df = pd.read_html(resp.content)
-    df = pd.DataFrame(df[0])
-    df['Ev']
 
+    df = pd.DataFrame(df[0])
+
+    df['EventName'] = data[2]
+
+    df['EventDateId'] = data[0]
+
+    df['EventId'] = data[1]
+
+    df['EventDate'] = data[3]
+
+    return df
 
 #scrape data
 url = 'https://ultrasignup.com/service/events.svc/closestevents?virtual=0&open=1&past=0&lat=0&lng=0&mi=500&mo=12&&m=1&dist=3,4,5,6,7'
@@ -31,15 +41,15 @@ url = 'https://ultrasignup.com/service/events.svc/closestevents?virtual=0&open=1
 resp = requests.get(url=url)
 data = resp.json()  # Check the JSON Response Content documentation below
 
-data = pd.DataFrame(data)
+event_data = pd.DataFrame(data)
 
 # get eventId
-event_id = [*map(get_eventId, data['EventDateId'])]
+event_id = [*map(get_eventId, event_data['EventDateId'])]
 event_id = pd.DataFrame(event_id)
-data = pd.merge(event_id, data)
+data = pd.merge(event_id, event_data)
+
+event_data.reset_index(inplace=True)
 
 # get entrant list
-resp = requests.get(url='https://ultrasignup.com/entrants_event.aspx?did=78606')
-df = pd.read_html(resp.content)
-df = pd.DataFrame(df[0])
+event_id = [*map(get_entrants, event_data, data['EventID'])]
 
